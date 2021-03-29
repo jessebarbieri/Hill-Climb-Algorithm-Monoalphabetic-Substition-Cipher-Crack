7db59917a41b93880dc7f986eccd69a5f43635c3
Jesse Barbieri <barbierijesse@gmail.com>
2021-03-29 16:24:55 -0400

    ngrams supported with decryption

--- decrypt.py
+++ decrypt.py
@@ -2,10 +2,9 @@ import os
 import sys
 import random
 import re
-from ngram import ngram
+from math import log10
 
 LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-fitness = ngram()
 
 
 def check_file():
@@ -31,6 +30,36 @@ solutions = {}
 solutions_attempt2 = {}
 
 
+def create_ngram():
+    ngram_dict = {}
+    for line in open('english_trigrams.txt'):
+        key, count, = line.split()
+        ngram_dict[key] = int(count)
+    length = len(key)
+    sum_val = sum(ngram_dict.values())
+
+    for key in ngram_dict.keys():
+        ngram_dict[key] = log10(float(ngram_dict[key]) / sum_val)
+
+    floor = log10(0.01 / sum_val)
+    return floor, ngram_dict, length
+
+
+ngram_floor, dict_ngram, length = create_ngram()
+
+
+def ngram_score(decrypted, ngram_floor, dict_ngram, length):
+    score = 0
+
+    for i in range(len(decrypted) - length + 1):
+        if decrypted[i:i + length] in dict_ngram:
+            score += dict_ngram.get(decrypted[i:i + length])
+        else:
+            score += ngram_floor
+
+    return score
+
+
 def hill_climb_algorithm():
     key_max = list(LETTERS)
     max_score = -99e9
@@ -38,22 +67,20 @@ def hill_climb_algorithm():
 
     iterations = 0
     count = 0
-    while iterations < 10:
+    while iterations < 8:
         random.shuffle(parentkey)  # randomly generate a key
-        #print(parentkey)
         deciphered = re.sub('[^A-Za-z0-9]+', '', decrypt_message(''.join(parentkey), message).upper())
-        #print(deciphered)
-        #print(len(deciphered))
-        parentscore = fitness.score(deciphered)
+        print(len(deciphered))
+        parentscore = ngram_score(deciphered, ngram_floor, dict_ngram, length)
 
-        while count < 2500:
+        while count < 5000:
             a = random.randint(0, 25)
             b = random.randint(0, 25)
             child = parentkey[:]
 
             child[a], child[b] = child[b], child[a]
             deciphered = re.sub('[^A-Za-z0-9]+', '', decrypt_message(''.join(child), message).upper())
-            score = fitness.score(deciphered)
+            score = ngram_score(deciphered, ngram_floor, dict_ngram, length)
 
             if score > parentscore:
                 parentscore = score
@@ -100,4 +127,3 @@ key_file.write(best_key)
 key_file.close()
 
 print(decrypt_message(best_key, message))
-
