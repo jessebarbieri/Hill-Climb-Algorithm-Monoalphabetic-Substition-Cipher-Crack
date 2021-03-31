b57bb454e2e468572d3f61e388abb754a34572c3
Jesse Barbieri <barbierijesse@gmail.com>
2021-03-31 13:23:48 -0400

    Ready for submission

--- PA2/decrypt.py
+++ PA2/decrypt.py
@@ -6,46 +6,38 @@ from math import log10
 
 LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-
+# Checks if our file is valid for decryption
 def check_file():
     input_file = sys.argv[1]
     if os.path.isfile(input_file):
         if input_file.endswith('.txt'):
             message_to_decrypt = open(input_file)
             message = message_to_decrypt.read()
-            ##print("File found and of correct file type.")
-            ##print("\nMessage to Decrypt:\n" + message)
             return message
 
         else:
-            print("Incorrect file type.")
-            print(input_file)
+            print("Incorrect file type -- files must been of type 'filename.txt'.")
+            sys.exit()
     else:
         print("File not found in the current working directory.")
+        sys.exit()
 
-
-message = check_file()
-solutions = {}
-
-
+# Creates our ngram dictionary for decryption comparison
 def create_ngram():
     ngram_dict = {}
-    for line in open('english_trigrams.txt'):
-        key, count, = line.split()
-        ngram_dict[key] = int(count)
-    key_length = len(key)
+    for line in open('Data/english_trigrams.txt'):
+        trigram, trigram_score, = line.split()
+        ngram_dict[trigram] = int(trigram_score)
+    key_length = 3
     sum_val = sum(ngram_dict.values())
 
-    for key in ngram_dict.keys():
-        ngram_dict[key] = log10(float(ngram_dict[key]) / sum_val)
+    for trigram in ngram_dict.keys():
+        ngram_dict[trigram] = log10(float(ngram_dict[trigram]) / sum_val)
 
     floor = log10(0.01 / sum_val)
     return floor, ngram_dict, key_length
 
-
-ngram_floor, dict_ngram, length = create_ngram()
-
-
+# Returns the score of found matches of decryption against our ngram dictionary
 def ngram_score(d_attempt, floor, ngram, l_key):
     score = 0
     for i in range(len(d_attempt) - l_key + 1):
@@ -56,39 +48,39 @@ def ngram_score(d_attempt, floor, ngram, l_key):
 
     return score
 
-
+# Runs the hill climb algorithm until best possible global maximum is found (within 8 iterations)
 def hill_climb_algorithm():
+    print("Cracking the code...")
     key_max = list(LETTERS)
     max_score = -99e9
-    parentscore, parentkey = max_score, key_max[:]
+    original_score, original_key = max_score, key_max[:]
 
     iterations = 0
     count = 0
     while iterations < 8:
-        random.shuffle(parentkey)  # randomly generate a key
-        deciphered = re.sub('[^A-Za-z0-9]+', '', decrypt_message(''.join(parentkey), message).upper())
-        print(len(deciphered))
-        parentscore = ngram_score(deciphered, ngram_floor, dict_ngram, length)
+        random.shuffle(original_key)  # randomly generates a key per iteration
+        deciphered = re.sub('[^A-Za-z0-9]+', '', decrypt_message(''.join(original_key), message).upper())
+        print("Iteration: " + str(iterations + 1) + "/8")
+        #print(len(deciphered))
+        original_score = ngram_score(deciphered, ngram_floor, dict_ngram, length)
 
         while count < 5000:
             a = random.randint(0, 25)
             b = random.randint(0, 25)
-            child = parentkey[:]
+            swapped_key = original_key[:]
 
-            child[a], child[b] = child[b], child[a]
-            deciphered = re.sub('[^A-Za-z0-9]+', '', decrypt_message(''.join(child), message).upper())
+            swapped_key[a], swapped_key[b] = swapped_key[b], swapped_key[a]
+            deciphered = re.sub('[^A-Za-z0-9]+', '', decrypt_message(''.join(swapped_key), message).upper())
             score = ngram_score(deciphered, ngram_floor, dict_ngram, length)
 
-            if score > parentscore:
-                parentscore = score
-                parentkey = child[:]
+            if score > original_score:
+                original_score = score
+                original_key = swapped_key[:]
                 count = 0
             count += 1
 
-        if parentscore > max_score:
-            max_score, key_max = parentscore, parentkey[:]
-            ##print("\nBest Score: " + max_score + " on iteration: " + i)
-            ##print("\nBest Key: " + "".join(key_max) + " Score: " + str(max_score) + " Iteration: " + str(iterations))
+        if original_score > max_score:
+            max_score, key_max = original_score, original_key[:]
             solutions.update({"".join(key_max): max_score})
 
         count = 0
@@ -96,7 +88,7 @@ def hill_climb_algorithm():
 
     return solutions
 
-
+# Print out of cryption, based on the original encryption provided by the professor
 def decrypt_message(key, cipher):
     de_translated = ''
     charsA = LETTERS
@@ -109,18 +101,22 @@ def decrypt_message(key, cipher):
             else:
                 de_translated += charsA[symIndex].lower()
         else:
-            # symbol is not in LETTERS, just add it
             de_translated += symbol
 
     return de_translated
 
-
+# Set our values and variables and run the algorithm
+ngram_floor, dict_ngram, length = create_ngram()
+message = check_file()
+solutions = {}
 solutions = hill_climb_algorithm()
-print(solutions)
+# Obtain the global maximum from our solutions dictionary
 best_key = max(solutions.keys(), key=(lambda k: solutions[k]))
 
+# Write our key to file
 key_file = open(os.path.join(os.getcwd(), 'key.txt'), 'w')
 key_file.write(best_key)
 key_file.close()
 
-print(decrypt_message(best_key, message))
+# Show the decryption to user
+#print(decrypt_message(best_key, message))
